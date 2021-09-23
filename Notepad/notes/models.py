0acfd2961bb29a11e2f97cfeb178e14e18d5a5e3
abdulhafeez <abodunrinafeezlekan@gmail.com>
2021-09-23 07:20:30 +0100

    updated df

--- Notepad/notes/models.py
+++ Notepad/notes/models.py
@@ -8,7 +8,7 @@ from Notepad.owners.models import Owner
 
 class Note(Model):
     id = models.AutoField(primary_key=True)
-    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Owner, on_delete=models.PROTECT)
     content = models.CharField(max_length=180)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     last_updated = models.DateTimeField(auto_now=True, null=True)
